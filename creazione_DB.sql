279ce07b10c1ab29c69d127387f3ddb9e86b6dca
michele nesler <michele.nesler@studenti.unipd.it>
2023-11-19 21:50:29 +0100

    Creato creazione_DB.sql + modifiche minori

---
+++ creazione_DB.sql
@@ -0,0 +1,43 @@
+drop table if exists Gift_card;
+drop table if exists Tipo_giftcard;
+drop table if exists Prenotazioni;
+drop table if exists Dati_cliente;
+drop table if exists Disponibilità;
+
+CREATE TABLE Tipo_giftcard(
+    Tipo VARCHAR(20) PRIMARY KEY,
+    Descrizione VARCHAR(500),
+    Utilizzi_possibili INT NOT NULL
+);
+
+CREATE TABLE Gift_card (
+	SHA_256 VARCHAR(256) PRIMARY KEY,
+	Utilizzi INT NOT NULL,
+	Tipo VARCHAR(20) NOT NULL,
+	FOREIGN KEY (Tipo) REFERENCES Tipo_giftcard(Tipo)
+);
+
+CREATE TABLE Dati_cliente(
+    ID INT PRIMARY KEY,
+    Email VARCHAR(50) NOT NULL,
+    Cellulare VARCHAR(15) NOT NULL,
+    Indirizzo VARCHAR(50),
+    Nome VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Prenotazioni(
+    Data_Ora_Inizio DATETIME PRIMARY KEY,
+    Tipo BOOLEAN NOT NULL,
+    Descrizione VARCHAR(500),
+    Utilizzi_possibili INT NOT NULL,
+    UNIQUE(Data_Ora_Inizio)
+);
+
+CREATE TABLE Disponibilità(
+    Data_Ora_Inizio DATETIME PRIMARY KEY CHECK (
+        Data_Ora_Inizio NOT IN (SELECT Data_Ora_Inizio FROM Prenotazioni)
+        AND ADDTIME(Data_Ora_Inizio, "-1:30:0") NOT IN (SELECT Data_Ora_Inizio FROM Prenotazioni WHERE Tipo = false)
+        ),
+);
+
+
