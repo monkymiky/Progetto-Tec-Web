d68810323f34d6f9c3df673692d04df2c2133f00
monkymiky <michines2001@gmail.com>
2023-12-02 06:17:46 +0100

    migliorato leggibilità codice

--- php/prenotazione.php
+++ php/prenotazione.php
@@ -15,29 +15,23 @@ $nome=;
 // se l'utente è gia presente nel db aggiunge solo la prenotazione, altrimenti entrambi. gestisce gli errori.
 // se l'email non è presente e fallisce la seconda query di inserimento (prenotazione) cancella i dati inseriti
 
-if (!$result1= $connessione->query("SELECT Email FROM Dati_cliente WHERE Email='$email'"))
-{
+if (!$result1= $connessione->query("SELECT Email FROM Dati_cliente WHERE Email='$email'")){
     echo "Errore della query: " . $connessione->error . ".";
-        exit();
-}else if ($result1->num_rows == 0) //email non presente nel DB
-    {
+    exit();
+}else if ($result1->num_rows == 0){ //email non presente nel DB
         if (!$result1 = $connessione->query("INSERT INTO Dati_cliente (Email,Cellulare,Indirizzo,Nome) VALUES ($email,$cel,$indirizzo,$nome);") 
-        || !$result2 = $connessione->query("INSERT INTO Prenotazioni (Data_Ora_Inizio,Tipo,InfoAggiuntive,cliente) VALUES ($DataOraInizio,$tipo,$Infoaggiuntive, $email);"))
-        {
+        || !$result2 = $connessione->query("INSERT INTO Prenotazioni (Data_Ora_Inizio,Tipo,InfoAggiuntive,cliente) VALUES ($DataOraInizio,$tipo,$Infoaggiuntive, $email);")){
             
             echo "Errore della query: " . $connessione->error . ".";
             exit();
-        }else{
-            if($result1){
-                if (!$result3 = $connessione->query("DELETE FROM Dati_cliente WHERE email = '$email';"))
-                {echo "Errore della query per il runback delle modifiche fatte prima del primo errore ($email): " . $connessione->error . ".";
-                exit();
+        }else if($result1){
+                if (!$result3 = $connessione->query("DELETE FROM Dati_cliente WHERE email = '$email';")){
+                    echo "Errore della query per il runback delle modifiche fatte prima del primo errore ($email): " . $connessione->error . ".";
+                    exit();
                 }
             }//else l'errore è sulla prima query --> non serve fare nulla.
-        }
          // elimino la tupla nella tabella Disponibilità con quell'orario
-        if (!$result3 = $connessione->query("DELETE FROM Disponibilità WHERE Data_Ora_Inizio = $DataOraInizio;"))
-        {
+        if (!$result3 = $connessione->query("DELETE FROM Disponibilità WHERE Data_Ora_Inizio = $DataOraInizio;")){
             echo "Errore della query per il runback delle modifiche fatte prima del primo errore ($DataOraInizio): " . $connessione->error . ".";
             exit();
         }
@@ -50,8 +44,7 @@ if (!$result1= $connessione->query("SELECT Email FROM Dati_cliente WHERE Email='
             }
         }
     }else{// email presente nel DB
-        if (!$result2 = $connessione->query("INSERT INTO Prenotazioni (Data_Ora_Inizio,Tipo,InfoAggiuntive,cliente) VALUES ();"))
-        {
+        if (!$result2 = $connessione->query("INSERT INTO Prenotazioni (Data_Ora_Inizio,Tipo,InfoAggiuntive,cliente) VALUES ();")){
             echo "Errore della query: " . $connessione->error . ".";
             exit();
         }
