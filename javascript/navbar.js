77f0046fb33a39e8068d33e7790d6991aa02c90d
monkymiky <michines2001@gmail.com>
2024-02-16 08:56:15 +0100

    update vari

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('menu-toggle');
     const menu = document.getElementById('menu');
+    const bradcrumb = document.getElementById('breadcrumb');
 
     menuToggle.addEventListener('click', function () {
         menu.classList.toggle('active');
+        bradcrumb.classList.toggle('active');
+        const element = document.getElementById('prestazioni');
+        if(element.getAttribute("aria-expanded") == "false") element.setAttribute("aria-expanded", "true"); 
+        else element.setAttribute("aria-expanded", "false"); 
     });
     
 });
