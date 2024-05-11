cf50209a17039c3d6adab2803d73327415a77493
monkymiky <michines2001@gmail.com>
2024-05-11 18:21:37 +0200

    script navbar debuggato, bottone orribile navbar eliminato freccetta aggiunta

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,14 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('menu-toggle');
     const menu = document.getElementById('menu');
-    const bradcrumb = document.getElementById('breadcrumb');
+    const breadcrumb = document.getElementById('breadcrumb');
 
     menuToggle.addEventListener('click', function () {
         menu.classList.toggle('active');
-        bradcrumb.classList.toggle('active');
-        const element = document.getElementById('prestazioni');
-        if(element.getAttribute(aria-expanded) == "false") element.setAttribute("aria-expanded", "true"); 
-        else element.setAttribute(aria-expanded, "false"); 
+        breadcrumb.classList.toggle('active');
+        if(menuToggle.getAttribute("aria-expanded") == "false") menuToggle.setAttribute("aria-expanded", "true"); 
+            else menuToggle.setAttribute("aria-expanded", "false"); 
+    });
+
+    var x = window.matchMedia("(max-width: 520px)");
+    const dropdownButton = document.getElementById('prestazioni');
+    if (x.matches) { // If media query matches
+        dropdownButton.setAttribute("aria-expanded", "true"); 
+      }
+    dropdownButton.addEventListener('click', function () {
+        if (!x.matches){
+            if(dropdownButton.getAttribute("aria-expanded") == "false") dropdownButton.setAttribute("aria-expanded", "true"); 
+            else dropdownButton.setAttribute("aria-expanded", "false"); 
+          }
+        
     });
     
+        
 });
