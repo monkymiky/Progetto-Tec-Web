3b1c80c95e532180ef66b22ad173aeb416890621
monkymiky <michines2001@gmail.com>
2024-05-11 12:21:02 +0200

    cose da fare aggiornato

--- php/controlli.php
+++ php/controlli.php
@@ -1,9 +1,9 @@
 <?php
 //se lo usi solo in prenotazioni.php copia e incolla
 function controllaInput(&$data) { // meglio htmlentities e sttrip_tags? 
-    $str = htmlspecialchars($data, ENT_QUOTES); // sostituisce i caratteri speciali con entità HTML tipo &minus
     $str = trim($data); // toglie spazi all'inizio e alla fine
     $str = stripslashes($data); // toglie i caratteri "/"
+    $str = htmlspecialchars($data, ENT_QUOTES); // sostituisce i caratteri speciali con entità HTML tipo &minus
   }
 
 function controllaDataOra(&$dataOra, &$messaggiForm){
