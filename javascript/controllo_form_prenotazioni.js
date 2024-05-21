3dc4dd73cc9605c8e8a075078378e73a3169002d
Luca Parise <lucaparise100@gmail.com>
2024-05-21 17:49:31 +0200

    fine controllo prenotazioni con js
    asdasdasd

--- javascript/controllo_form_prenotazioni.js
+++ javascript/controllo_form_prenotazioni.js
@@ -3,6 +3,9 @@ var email_regex = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-]{2,})+.)+([a-zA-Z0-9]{2,})+$
 var nome_regex = /^[a-zA-Z0-9\s]+$/;
 var regex = /^[a-zA-Z0-9\s.,?!@#&$%*()+-]+$/;
 let cell_regex = /^\d{0,15}$/;
+var numero_carta_regex = /^(\d{15}|\d{16})$/;
+var numero_CVV_regex = /^(\d{3})$/;
+
 
 //variabili per il controllo 
 var form = document.getElementById('formPrenota');
@@ -65,12 +68,10 @@ function checkIndirizzo() {
         return true;
     }
 };
-
 function checkNomeCarta() {
-    console.log("nome della carta inserito = " + elNomeCarta.value);
-    err = document.getElementById('errPrenotazioniNomeCognome');
-    noerr = document.getElementById('noErrPrenotazioniNomeCognome');
-    if(elNomeCarta.value=="" || elNomeCarta==undefined || !nome_regex.test(elNomeCarta.value)) {
+    err = document.getElementById('errPrenotazioniNomeCarta');
+    noerr = document.getElementById('noErrPrenotazioniNomeCarta');
+    if(elNomeCarta=="" || elNomeCarta == undefined || !nome_regex.test(elNomeCarta.value)) {
         err.style.display = 'inline';
         noerr.style.display = 'none';
         return false;
@@ -80,7 +81,51 @@ function checkNomeCarta() {
         noerr.style.display = 'inline';
         return true;
     }
-};
+}
+function checkNumeroCarta() {
+    err = document.getElementById("errPrenotazioniNumeroCarta");
+    noerr = document.getElementById("noErrPrenotazioniNumeroCarta");
+    if(elNumeroCarta.value=="" || elNumeroCarta==undefined || !numero_carta_regex.test(elNumeroCarta.value)) {
+        err.style.display = 'inline';
+        noerr.style.display = 'none';
+        return false;
+    }
+    else {
+        err.style.display = 'none';
+        noerr.style.display = 'inline';
+        return true;
+    }
+}
+function checkCVV() {
+    err = document.getElementById('errPrenotazioniCVV');
+    noerr = document.getElementById('noErrPrenotazioniCVV');
+    if(elCVVCarta.value=="" || elCVVCarta.value==undefined || !numero_CVV_regex.test(elCVVCarta.value)) {
+        err.style.display = 'inline';
+        noerr.style.display = 'none';
+        return false;
+    }
+    else {
+        err.style.display = 'none';
+        noerr.style.display = 'inline';
+        return true;
+    }
+}
+
+function checkGenerale() {
+    var A = checkEmail();
+    var B = checkCell();
+    var C = checkIndirizzo();
+    var D = checkNomeCarta();
+    var E = checkNumeroCarta();
+    var F = checkCVV;
+    if(A && B && C && D && E && F) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 
 function showCardData() {
     document.getElementById("cardDataFieldset").classList.toggle("hiddenObj");
@@ -90,3 +135,16 @@ elEmail.addEventListener('blur', checkEmail, false);
 elCell.addEventListener('blur', checkCell, false);
 elIndirizzo.addEventListener('blur', checkIndirizzo, false);
 elNomeCarta.addEventListener('blur', checkNomeCarta, false);
+elNumeroCarta.addEventListener('blur', checkNumeroCarta, false);
+elCVVCarta.addEventListener('blur', checkCVV, false);
+
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+    if(checkGenerale()) {
+        form.submit();
+    }
+    else {
+        alert("Controlla i campi");
+        elEmail.focus();
+    }
+})
