a60d6e34c767f5f3d8f93577e630850cb0fe5b87
monkymiky <michines2001@gmail.com>
2024-05-29 06:53:13 +0200

    inserite immagini mancanti, sostituite ridondanti

--- php/prenotazioni.php
+++ php/prenotazioni.php
@@ -73,17 +73,17 @@ $paginaHTML = str_replace("{messaggiForm}", $messaggiForm, $paginaHTML);
 
 //------------------------------------- clanedario ----------------------------------------------
 $stringMese = "0";
-    /**if(!empty($_POST['action'])){
+    if(!empty($_POST['action'])){
         controllaInput($_POST["action"]);
         $calendario = new Calendario(false,$_POST["action"]);
         $paginaHTML = str_replace("{calendario}", $calendario->getStringaCalendario(), $paginaHTML);
         $paginaHTML = str_replace("{slot}", $calendario->getStringaSlot(), $paginaHTML);
         $stringMese = $_POST['action'];
-    }else{**/
+    }else{
         $calendario = new Calendario(false, 0);
         $paginaHTML = str_replace("{calendario}", $calendario->getStringaCalendario(), $paginaHTML);
         $paginaHTML = str_replace("{slot}", $calendario->getStringaSlot(), $paginaHTML);
-   // }
+    }
     $paginaHTML = str_replace("{mese}", $stringMese, $paginaHTML);
 
 echo $paginaHTML;
