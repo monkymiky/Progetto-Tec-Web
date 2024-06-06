721b7bf59fa969cff03b9ff1195caa08d1132720
Luca Parise <lucaparise100@gmail.com>
2024-06-06 17:13:08 +0200

    Update dati_Prenotazione.sql

--- dati_Prenotazione.sql
+++ dati_Prenotazione.sql
@@ -15,3 +15,12 @@ INSERT INTO Prenotazioni (Data_Ora_Inizio, InfoAggiuntive, cliente, tipo) VALUES
 ('2024-07-09 10:00:00', 'SomeInfo', 'dennisdaniels@google.it', 1);
 
 
+INSERT INTO NonDisponibili (Data_Ora_Inizio) VALUES 
+('2024-07-01 16:00:00'),
+('2024-07-02 08:30:00'),
+('2024-07-03 10:00:00'),
+('2024-07-04 14:30:00'),
+('2024-07-05 19:00:00'),
+('2024-07-07 11:30:00'),
+('2024-07-07 17:30:00'),
+('2024-07-09 10:00:00');
