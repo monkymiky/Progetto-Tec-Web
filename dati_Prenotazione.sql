e557de2ec8f6cfd7a6f3c8197dbaf31e004afa99
Luca Parise <lucaparise100@gmail.com>
2024-06-06 16:26:28 +0200

    Create dati_Prenotazione.sql

---
+++ dati_Prenotazione.sql
@@ -0,0 +1,17 @@
+INSERT INTO Prenotazioni (Data_Ora_Inizio, InfoAggiuntive, cliente, tipo) VALUES
+('2024-07-01 16:00:00', 'SomeInfo', 'mageekey1340@google.it', 1),
+
+('2024-07-02 08:30:00', 'SomeInfo', 'zacheryavila2914@google.it', 1),
+
+('2024-07-03 10:00:00', 'SomeInfo', 'whoopivalencia@google.it', 0),
+
+('2024-07-04 14:30:00', 'SomeInfo', 'orsonwilliamson8999@google.it', 1),
+
+('2024-07-05 19:00:00', 'SomeInfo', 'dennisdaniels@google.it', 1),
+
+('2024-07-07 11:30:00', 'SomeInfo', 'bradyowen@google.it', 0),
+('2024-07-07 17:30:00', 'SomeInfo', 'bradyowen@google.it', 1),
+
+('2024-07-09 10:00:00', 'SomeInfo', 'dennisdaniels@google.it', 1);
+
+
