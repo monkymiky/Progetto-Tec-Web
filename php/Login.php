54c0df4bd31f1051d41aad629accc49857ff4eee
monkymiky <michines2001@gmail.com>
2024-06-10 16:48:34 +0200

    debug login

--- php/Login.php
+++ php/Login.php
@@ -12,39 +12,40 @@ setlocale(LC_ALL, 'it_IT');
 
 $paginaHTML = file_get_contents("../html/loginTemplate.html");
 
-$messaggiForm = "";
+$messaggiForm = " messaggi debug : \n";
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 $extra = 'admin.php';
 
 if(isset($_SESSION["session_id"])){ // login gia avvenuto?
+  $messaggiForm .= "\n debug : sessione aperta ";
   header("Location: http://$host$uri/$extra");// redirect ad admin.php
   exit;
 }else{ // login da fare
   if (isset($_POST['loginSubmit'])) {
-    $messaggiForm .= " debug : if 1  ";
+    $messaggiForm .= " \n debug : if 1  ";
     if(!isset($_POST["user"]) && !isset($_POST["password"])){
-      $messaggiForm .= "  if 2  ";
+      $messaggiForm .= "\n  if 2  ";
       controllaInput($_POST["user"]) ; 
       controllaInput($_POST["password"]);
       $connessione = new DBAccess();
       $connessione->openDBConnection();
       if ($connessione->login($_POST['user'],$_POST['password'])){
-        $messaggiForm .= "  if 3  ";
+        $messaggiForm .= "\n  if 3  ";
         $connessione->closeConnection();
         $_SESSION["session_id"] = session_id();
         header("Location: http://$host$uri/$extra");// redirect ad admin.php
         exit;
       }else {
-        $messaggiForm .= " else 1  ";
+        $messaggiForm .= " \nelse 1  ";
         $connessione->closeConnection(); 
       }
     }else{
-      $messaggiForm .= "user o password non inserito";
+      $messaggiForm .= "\n user o password non inserito";
     }
-    $messaggiForm .= " out 1  ";
+    $messaggiForm .= " \n out 1  ";
   }
-  $messaggiForm .= " out 2  ";
+  $messaggiForm .= " \n out 2  ";
 }
 
 $paginaHTML = str_replace("{messaggiForm}", $messaggiForm, $paginaHTML);
