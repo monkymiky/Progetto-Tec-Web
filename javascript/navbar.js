e04e63e446bad910f133af5987cf1c295778a513
Luca Parise <lucaparise100@gmail.com>
2024-06-23 16:01:55 +0200

    sempre navbar.js problems
    a quanto pare esisteva l'id per il bottone. Solo non esisteva in tutti i file. Mi sono assicurato che esista un solo id per ogni pagina html per il bottone della navbar

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,29 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Riferimenti agli elementi
     const menuToggle = document.getElementById('menu-toggle');
     const menu = document.getElementById('menu');
     const breadcrumb = document.getElementById('breadcrumb'); 
     var x = window.matchMedia("(max-width: 520px)");
     const dropdownButton = document.getElementById('dropdown_button');
     
-    menuToggle.addEventListener('click', function () {
-        menu.classList.toggle('active');
-        breadcrumb.classList.toggle('active');
-        if(menuToggle.getAttribute("aria-expanded") == "false") menuToggle.setAttribute("aria-expanded", "true"); 
-            else menuToggle.setAttribute("aria-expanded", "false"); 
-    });
-
-    
-    if (x.matches) { // If media query matches
-        dropdownButton.setAttribute("aria-expanded", "true"); 
+    // Controlla se gli elementi esistono prima di aggiungere event listeners
+    if (menuToggle && menu && breadcrumb) {
+        menuToggle.addEventListener('click', function () {
+            menu.classList.toggle('active');
+            breadcrumb.classList.toggle('active');
+            if (menuToggle.getAttribute("aria-expanded") == "false") {
+                menuToggle.setAttribute("aria-expanded", "true"); 
+            } else {
+                menuToggle.setAttribute("aria-expanded", "false"); 
+            }
+        });
+    } else {
+        console.error("menuToggle, menu, o breadcrumb non trovati nel DOM.");
     }
 
-    dropdownButton.addEventListener('click', function () {
-        if (!x.matches){
-            if(dropdownButton.getAttribute("aria-expanded") == "false") dropdownButton.setAttribute("aria-expanded", "true"); 
-            else dropdownButton.setAttribute("aria-expanded", "false"); 
-          }
-        
-    });
-    
-        
+    if (dropdownButton) {
+        if (x.matches) { // Se la media query corrisponde
+            dropdownButton.setAttribute("aria-expanded", "true"); 
+        }
+
+        dropdownButton.addEventListener('click', function () {
+            if (!x.matches) {
+                if (dropdownButton.getAttribute("aria-expanded") == "false") {
+                    dropdownButton.setAttribute("aria-expanded", "true"); 
+                } else {
+                    dropdownButton.setAttribute("aria-expanded", "false"); 
+                }
+            }
+        });
+    } else {
+        console.error("dropdownButton non trovato nel DOM.");
+    }
 });
