5fbb6b518d6317e43ca4542e3ea2554242c3962f
eLeo00 <120917946+eLeo00@users.noreply.github.com>
2024-09-01 11:10:29 +0200

    aggiunta function minore "emphasize"

--- javascript/prenotazioni_calendario.js
+++ javascript/prenotazioni_calendario.js
@@ -89,3 +89,12 @@ function riempiData(object) { //riempie i campi dati del form con le informazion
 function _resetTarget() { //se sono visualizzati degli slot e viene cambiata la tipologia studio/domicilio, gli slot spariscono
     window.location.href = "#domicilio-domicilio-studioSelection"; //potrebbe dare problemi di accessibilità (maledetti screenreader)
 }
+
+function emphasize(object) {
+	document.querySelectorAll('.emphasized').forEach (
+    	function(obj) {
+        	obj.classList.remove('emphasized');
+        }
+    )
+	object.classList.add('emphasized');
+}
